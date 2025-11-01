7fde211e9396cc62c7a9f67db6fc154d522da7fa
Shayan Fareed <sfx.deve@gmail.com>
2025-11-01 11:17:24 +0500

    Add Todo functionality with CRUD operations and integrate Radix UI dialog components

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,11 +1,13 @@
 import type { RouterClient } from "@orpc/server";
 import { publicProcedure } from "../index";
 import { bankRouter } from "./bank";
+import { todoRouter } from "./todo";
 
 export const appRouter = {
 	healthCheck: publicProcedure.handler(() => {
 		return "OK";
 	}),
+	todo: todoRouter,
 	bank: bankRouter,
 };
 
