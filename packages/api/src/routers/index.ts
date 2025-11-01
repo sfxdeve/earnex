89173e16ef700b3b3096e6882e5c5ff940126d97
Shayan Fareed <sfx.deve@gmail.com>
2025-11-01 09:22:11 +0500

    Refactor payment processing: remove payment router and related models; update User model structure

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,12 +1,10 @@
 import type { RouterClient } from "@orpc/server";
 import { publicProcedure } from "../index";
-import { paymentRouter } from "./payment";
 
 export const appRouter = {
 	healthCheck: publicProcedure.handler(() => {
 		return "OK";
 	}),
-	payment: paymentRouter,
 };
 
 export type AppRouter = typeof appRouter;
